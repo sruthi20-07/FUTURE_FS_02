324920459b00a22fd610c29e48b34b98185aae5a
Sruthi <vindulasruthi20@gmail.com>
2026-02-11 22:25:54 +0530

    Initial commit - Mini CRM project

---
+++ backend/routes/leadRoutes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const Lead = require("../models/Lead");
+
+// ➕ Add new lead
+router.post("/", async (req, res) => {
+  try {
+    const lead = new Lead(req.body);
+    await lead.save();
+    res.status(201).json(lead);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// 📄 Get all leads
+router.get("/", async (req, res) => {
+  try {
+    const leads = await Lead.find().sort({ createdAt: -1 });
+    res.json(leads);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// ✏️ Update lead
+router.put("/:id", async (req, res) => {
+  try {
+    const updated = await Lead.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.json(updated);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// ❌ Delete lead (optional)
+router.delete("/:id", async (req, res) => {
+  try {
+    await Lead.findByIdAndDelete(req.params.id);
+    res.json({ message: "Lead deleted" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
